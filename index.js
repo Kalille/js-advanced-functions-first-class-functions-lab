e1495b11a8b8153828d6459394c044ea847ddc68
kalille Overton <>
2021-04-22 21:21:14 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia']
+const returnFirstTwoDrivers = function(){
+ return drivers.slice(0,2);
+   
+}
+
+
+ const returnLastTwoDrivers = function(){
+     return drivers.slice(-2);
+ }
+//  function selectingDrivers(){
+//     returnFirstTwoDrivers()
+//     returnLastTwoDrivers()
+// }
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(fare){
+    return function(){
+        return fare * 2
+    
+}
+}
